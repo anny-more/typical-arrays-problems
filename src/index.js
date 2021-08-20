c830b16077313285e4e876dba91c4d689ba2012b
anny_more <anxxxen@ya.ru>
2021-08-20 08:04:42 +0300

    fix functions for empty array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-  if (array.length < 1) {
+  if (typeof array === 'undefined' || array.length < 1) {
     return 0;
   }
   let less = array[0];
@@ -11,7 +11,7 @@ exports.min = function min (array) {
 };
 
 exports.max = function max (array) {
-  if (array.length < 1) {
+  if (typeof array === 'undefined' || array.length < 1) {
     return 0;
   }
   let big = array[0];
@@ -22,7 +22,7 @@ exports.max = function max (array) {
 };
 
 exports.avg = function avg (array) {
-  if (array.length < 1) {
+  if (typeof array === 'undefined' || array.length < 1) {
     return 0;
   }
   let sum = 0;
